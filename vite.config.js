5c3d883b072243dfec185c344d93c1e04406fd99
jlumbroso <jlumbroso@users.noreply.github.com>
2025-02-21 22:08:07 +0000

    deploy: 1ec58b9c57aefe25b627209d943ef6b7d3528108

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-
-export default defineConfig({
-  // If your GitHub Pages URL is https://<username>.github.io/student-dashboard/,
-  // the base must match:
-  base: '/student-dashboard/',
-  plugins: [svelte()]
-});
